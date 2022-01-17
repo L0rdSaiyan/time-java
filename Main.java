d5f7bad1086a538b53a52cabf4336bd9288597bc
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-01-17 18:10:06 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.time.LocalTime;
+public class Main
+{
+    public static void main(String[] args) {
+  
+    LocalTime hora = LocalTime.now();
+    
+    System.out.println(hora);
+
+  }
+}
